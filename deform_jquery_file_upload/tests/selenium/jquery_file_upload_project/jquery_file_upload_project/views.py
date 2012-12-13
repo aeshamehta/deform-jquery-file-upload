3b53f6a389c573aa1cbfbe8263d6139f543cf715
Mariano <mariano.mara@gmail.com>
2012-12-13 08:19:33 -0300

    tests

--- deform_jquery_file_upload/tests/selenium/jquery_file_upload_project/jquery_file_upload_project/views.py
+++ deform_jquery_file_upload/tests/selenium/jquery_file_upload_project/jquery_file_upload_project/views.py
@@ -1,6 +1,21 @@
+import colander
+import deform
+import deform_jquery_file_upload
 from pyramid.view import view_config
 
+tmpstore = {}
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'jquery_file_upload_project'}
+
+@view_config(route_name="test", renderer='templates/test.pt')
+def test(request):
+    class Schema(colander.Schema):
+        upload = colander.SchemaNode(
+                deform.FileData(),
+                widget=deform_jquery_file_upload.JqueryFileUploadWidget(tmpstore)
+                )
+    schema = Schema()
+    form = deform.Form(schema, buttons=('submit',))
+    return render_form(form, success=tmpstore.clear)
